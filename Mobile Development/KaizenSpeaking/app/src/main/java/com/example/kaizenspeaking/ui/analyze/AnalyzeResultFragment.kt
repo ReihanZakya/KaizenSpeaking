eeae0925fa01b6aad95000a9bac8be81f7c9e493
ReihanZakya <rezakya41@gmail.com>
2024-11-15 22:38:45 +0700

    add bar chart sample

--- Mobile Development/KaizenSpeaking/app/src/main/java/com/example/kaizenspeaking/ui/analyze/AnalyzeResultFragment.kt
+++ Mobile Development/KaizenSpeaking/app/src/main/java/com/example/kaizenspeaking/ui/analyze/AnalyzeResultFragment.kt
@@ -8,6 +8,11 @@ import android.view.ViewGroup
 import com.example.kaizenspeaking.R
 import com.example.kaizenspeaking.databinding.FragmentAnalyzeBinding
 import com.example.kaizenspeaking.databinding.FragmentAnalyzeResultBinding
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 
 
 class AnalyzeResultFragment : Fragment() {
@@ -16,6 +21,7 @@ class AnalyzeResultFragment : Fragment() {
 
     private val binding get() = _binding!!
 
+    private lateinit var barChart: BarChart
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +34,37 @@ class AnalyzeResultFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Initialize BarChart
+        barChart = binding.barchart
+
+        // Data for the chart
+        val entries = ArrayList<BarEntry>()
+        entries.add(BarEntry(0f, 4f)) // Data point 1
+        entries.add(BarEntry(1f, 2f)) // Data point 2
+        entries.add(BarEntry(2f, 6f)) // Data point 3
+        entries.add(BarEntry(3f, 3f)) // Data point 4
+
+        // Create BarDataSet
+        val barDataSet = BarDataSet(entries, "Bar Chart Example")
+
+        // Set different colors for each bar
+        val colors = arrayListOf(
+            resources.getColor(android.R.color.holo_blue_light),
+            resources.getColor(android.R.color.holo_green_light),
+            resources.getColor(android.R.color.holo_orange_light),
+            resources.getColor(android.R.color.holo_red_light)
+        )
+        barDataSet.colors = colors
+
+        // Create BarData
+        val barData = BarData(barDataSet)
+
+        // Set data to the chart
+        barChart.data = barData
+
+        // Refresh chart to render the new data
+        barChart.invalidate()
     }
 
     override fun onDestroyView() {
