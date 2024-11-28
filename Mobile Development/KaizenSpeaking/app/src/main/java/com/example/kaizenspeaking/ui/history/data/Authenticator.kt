14600c4a3656cbcaa08e6bda1e495b487a956586
Khoirul Anam <khoirulanam1604@mail.ugm.ac.id>
2024-11-29 02:20:37 +0700

    CHANGE authenticator to new anam account

--- Mobile Development/KaizenSpeaking/app/src/main/java/com/example/kaizenspeaking/ui/history/data/Authenticator.kt
+++ Mobile Development/KaizenSpeaking/app/src/main/java/com/example/kaizenspeaking/ui/history/data/Authenticator.kt
@@ -1,6 +1,9 @@
 package com.example.kaizenspeaking.ui.history.data
 
 object Authenticator {
-    const val TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJrb3NvbmciLCJ1c2VyX2lkIjoiOTI1ZjQyNGUtZTk2My00N2Y1LWE4OGMtNmQ4OTNkZjU4YWI4Iiwicm9sZSI6InVzZXIiLCJleHAiOjE3MzUxNDY5MzN9.50SgQfLldCPsfn1mefNgupjC7nZngb8gOspkzxeOMoE"
-    const val USER_ID = "925f424e-e963-47f5-a88c-6d893df58ab8"
+    const val TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ4ajQ3OGJndyIsInVzZXJfaWQiOiI3N2M3ZDYwNC00NDU3LTQ0OTYtOTEzMS1lMzZhZTFkODlkNjgiLCJyb2xlIjoidXNlciIsImV4cCI6MTczNTQxMTY2NH0.H_IikR18LAM9Qg_WzYEnMC18vxMmM0psy7e2le-oQts"
+    const val USER_ID = "77c7d604-4457-4496-9131-e36ae1d89d68"
+    const val EMAIL = "anam@mail.id"
+    const val PASSWORD = "anam1234"
+    const val DEVICE_ID = "5327b5c7f7ac05d8"
 }
